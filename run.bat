7edfdc35061e869950da25a061e4846e39a07224
Kodjo <kodjo.klouvi@gmail.com>
2020-06-06 11:23:18 +0200

    Implement json configuration file

---
+++ run.bat
@@ -0,0 +1,1 @@
+spark-submit --master $1   --deploy-mode cluster   --executor-cores 7   --executor-memory 14G   --files ./config.json   --class FlightOnTimeMain   target/scala-2.12/flightsontime_klouvi_riva_2.12-1.0.jar
